565ceb4934fc7ab00182304fe198e4ae97454a05
Alan Pope <alan.pope@anchore.com>
2025-04-11 11:22:29 +0100

    Enable local settings if available

--- savannah/settings.py
+++ savannah/settings.py
@@ -247,3 +247,15 @@ GITHUB_SCOPE = "read:org,public_repo"
 TOTD_EXCLUDE_NS = ['admin']
 
 DJSTRIPE_WEBHOOK_EVENT_CALLBACK = "billing.callbacks.stripe_event_callback"
+
+##################
+# LOCAL SETTINGS #
+##################
+
+# Allow any settings to be defined in local_settings.py which should be
+# ignored in your version control system allowing for settings to be
+# defined per machine.
+try:
+    from local_settings import *
+except ImportError:
+    pass
